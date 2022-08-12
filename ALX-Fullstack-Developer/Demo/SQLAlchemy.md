de1bbadfc9d7fbce23b6687c402c16aa87fb00f9
Sephens <ombamstephodhis@gmail.com>
2022-08-12 12:25:53 +0300

    more sqlalchemy

--- ALX-Fullstack-Developer/Demo/SQLAlchemy.md
+++ ALX-Fullstack-Developer/Demo/SQLAlchemy.md
@@ -23,3 +23,17 @@ The name of the host location where your database is located.
 For us our database server is located on our local machine. So we will just pass it in either localhost or 127.0.0.1.
 Specify which port you'd like to connect to by default, 5432 for database connections.
 Finally, followed by a slash and then specify the name of the particular database that exists on the server located at this host and connecting on this port, logging in with this username and password, and using this particular database system.
+
+### Running the flask app: Terminal (option 1)
+Make sure you are in the directory that contains app.py
+We run a flask app defined at app.py by running this line of code on one line
+```FLASK_APP=app.py FLASK_DEBUG=true flask run```
+```FLASK_APP``` must be set to the server file path with an equal sign in between. No spaces. ```FLASK_APP = app.```py will not work.
+```FLASK_DEBUG=true``` will enable debug mode which allows live reload
+```flask run``` actually executes the flask server code in the ```app.py``` file
+
+
+### Running the flask app: Python (option 2)
+Make sure you are in the directory that contains app.py
+Do not enter any of the flask code mentioned in option 1
+Simply include the following in your python file
--- ALX-Fullstack-Developer/Demo/flask-hello-app.py
+++ ALX-Fullstack-Developer/Demo/flask-hello-app.py
@@ -1,18 +1,39 @@
 import imp
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import false, true
 
 # instantiate the application
 app = Flask(__name__)
-# link an instance of a db that we can use in our app
-db = SQLAlchemy(app)
-# set a configuration variable and set it to a string that the db will understand
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
 # enable the app to listen to the home page(the root route)
+# @app.route is a Python decorator.
 @app.route('/')
+
 # define some methods that will act as the route handler
 def index():
-    return('Hello world')
+    return('Hello Steve')
+
+
+# # link an instance of a db that we can use in our app
+# db = SQLAlchemy(app)
+# # set a configuration variable and set it to a string that the db will understand
+# passw = '  x'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:%(passw)s@localhost:5432/example'
+
+# # lets create a person class that will inherit from db.model
+# class Person(db.Model):
+#     __tablename__ = 'todos'
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(), nullable = False)
+
+
+# # create a table using db.create_all() method
+# #it detects the models and creates the tables for them automatially
+# db.create_all()
+
+
+
 
 # Run a flask application in terminal by:
 # FLASK_APP=app_name flask run
